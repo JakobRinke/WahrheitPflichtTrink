6d59b7528dd80d35f74672606ec6e53f2794bf3b
JakobRinke <61539310+JakobRinke@users.noreply.github.com>
2021-04-20 10:39:47 +0200

    Update main.js

--- main.js
+++ main.js
@@ -68,12 +68,12 @@ Array.prototype.random = function () {
 function getAufgabe()
 {
     var aufgabe = aufgaben[Math.floor(Math.random() * aufgaben.length)];
-    var p1 = Math.floor(Math.random() * (aufgaben.length-1)-1);
+    var p1 = Math.floor(Math.random() * (playerList.length));
     var p2 = p1;
     while (p1==p2)
     {
         console.log(p2);
-        p2 = Math.floor(Math.random() * (aufgaben.length-1)-1);
+        p2 = Math.floor(Math.random() * (playerList.length));
     }
     return aufgabe.replace(/\[1\]/, playerList[p1]).replace(/\[2\]/, playerList[p2]);
 }
