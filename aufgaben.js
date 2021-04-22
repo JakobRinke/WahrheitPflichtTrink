fe0fd45aefdb5fad8b2a28969015de4e1015c25c
JakobRinke <61539310+JakobRinke@users.noreply.github.com>
2021-04-22 16:59:42 +0200

    Luca der Hund

--- aufgaben.js
+++ aufgaben.js
@@ -2,5 +2,8 @@ const aufgaben = [
     "[1] muss 2 Schlücke trinken.",
     "Jeder der schon einmal mit [1] Schach gespielt hat muss 4 Schlücke trinken.",
     "[1] sagt 2 Wahrheiten und eine Lüge über sich. Die anderen müssen erraten, was die Lüge ist. Diejenigen die Falsch liegen, müssen 5 Schlücke trinken.",
-    "[1] spielt Schere Stein Papier gegen [2]. Der Verlierer muss 3 Schlücke trinken."
+    "[1] spielt Schere Stein Papier gegen [2]. Der Verlierer muss 3 Schlücke trinken.",
+	"[1] muss sich drehen bis er/sie umfällt",
+	"[1] macht Armdrücken mit [2]. Der Verlierer trinkt 5 Schlücke",
+	"[1] schaut seinem linken Sitznachbar für die nächsten 3 Aufgaben in die Augen"
 ]
